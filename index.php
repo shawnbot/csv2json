092d6fe3ceb53b4e4bfd759acf83a5da5b81cf72
Shawn Allen <shawn@stamen.com>
2010-07-19 10:14:26 -0400

    comments

--- index.php
+++ index.php
@@ -8,24 +8,25 @@ function option($name, $default) {
   return isset($_POST[$name]) ? $_POST[$name] : $default;
 }
 
+// adapted from: http://php.net/filesize
 function pretty_size($bytes, $precision=1) { 
-    $units = array('b', 'KB', 'MB', 'GB', 'TB'); 
+  $units = array('b', 'KB', 'MB', 'GB', 'TB'); 
 
-    $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-    $pow = min($pow, count($units) - 1);
+  $bytes = max($bytes, 0);
+  $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+  $pow = min($pow, count($units) - 1);
 
-    $bytes /= pow(1024, $pow);
+  $bytes /= pow(1024, $pow);
 
-    return sprintf('%s %s', round($bytes, $precision), $units[$pow]);
+  return sprintf('%s %s', round($bytes, $precision), $units[$pow]);
 }
 
-ob_start();
-
+// if using an uploaded file, grab its temp file name
 if (isset($_FILES['upload']) && !empty($_FILES['upload']['name'])) {
 
   $filename = $_FILES['upload']['tmp_name'];
 
+// if using pasted text, write it to a temp file
 } else if ($_POST['csv']) {
 
   $filename = tempnam('/tmp', 'upload-csv');
@@ -35,6 +36,7 @@ if (isset($_FILES['upload']) && !empty($_FILES['upload']['name'])) {
 
 }
 
+// our conversion options
 $delimiter = option('delimiter', ',');
 $quotechar = option('quotechar', '"');
 $indent = option('indent', 0);
@@ -50,17 +52,23 @@ if ($filename) {
                 'v' => $variable);
   $options = '';
   foreach ($opts as $o => $v) {
+    // the quotechar argument can be empty
     if (!empty($v) || $o == 'q')
       $options .= sprintf(' -%s %s', $o, escapeshellarg($v));
   }
+  // shell out the conversion to csv2json.py, capturing stdout
   $cmd = "python csv2json.py $options $filename";
   $json = `$cmd`;
 
+  // if we're downloading, send the appropriate headers
   if (!empty($json) && $download) {
     header('Content-Type: application/json');
     header('Content-Disposition: attachment; filename=' . $output_filename);
   }
 }
+
+// buffer output so that we can send an accurate Content-Length header
+ob_start();
 
 if ($download == true):
 
@@ -139,8 +147,10 @@ else:
 
 endif;
 
+// grab the buffer, send the Content-Length header, and print
 $output = ob_get_contents();
 ob_end_clean();
+// XXX: not sure if we want mb_strlen() here or just strlen()...
 header(sprintf('Content-Length: %d', mb_strlen($output)));
 print $output;
 
